a4ee0960d2b1df6e43398d3f3944fec7b68a3a90
Chelovekdenis <chelovekdenis@yandex.ru>
2020-10-02 19:04:14 +0300

    Install

--- test/server.js
+++ test/server.js
@@ -2,18 +2,27 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const redis = require("redis")
+const client = redis.createClient()
+
+client.on("error", function(error) {
+    console.error(error)
+})
+
+client.set("user1",
+    Buffer.from("user1.123." + process.env.ACCESS_TOKEN_SECRET).toString('base64'),
+    redis.print)
+
+client.set("user2",
+    Buffer.from("user2.1234." + process.env.ACCESS_TOKEN_SECRET).toString('base64'),
+    redis.print)
+
+client.set("user3",
+    Buffer.from("user3.12345." + process.env.ACCESS_TOKEN_SECRET).toString('base64'),
+    redis.print)
+
 
 app.use(express.json())
-
-const users = [
-    {
-        username: 'Kyle'
-    },
-    {
-        username: 'Jim'
-    }
-]
-
 
 app.get('/users', authenticateToken, (req, res) => {
     // res.json(users.filter(post => post.username === req.user.name))
@@ -22,38 +31,61 @@ app.get('/users', authenticateToken, (req, res) => {
     res.end(`username: `)
 })
 
+
+
 app.post('/login', (req, res) => {
+
+    // TODO validation
+
     const username = req.body.username
     const password = req.body.password
     // const user = { name: username }
 
-    console.log(req.body)
+    client.get(username, (err, replay) => {
+        let user = Buffer.from(replay, 'base64').toString('ascii')
+        if (user.split('.')[1] === password) {
+            console.log("password success")
+        }
+
+
+    })
+
 
     // const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
+
     let accessToken = username + "." + password + "." + process.env.ACCESS_TOKEN_SECRET
     accessToken = Buffer.from(accessToken).toString('base64')
+
+    // client.set(req.body.username, accessToken, redis.print)
 
     res.json({ accessToken: accessToken })
 })
 
 function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    console.log("AH -> " + authHeader)
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token == null) return res.sendStatus(401)
+    // const authHeader = req.headers['authorization']
+    // console.log("AH -> " + authHeader)
+    // const token = authHeader && authHeader.split(' ')[1]
+    // if (token == null) return res.sendStatus(401)
 
-    console.log(token)
+    // console.log(token)
 
-    let user = Buffer.from(token, 'base64').toString('ascii')
+    client.get("token", redis.print)
+
+    // let user = Buffer.from(token, 'base64').toString('ascii')
 
 
     // jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(user)
+    // console.log(user)
     // if (err) return res.sendStatus(403)
-    req.user = user
+    // req.user = user
     next()
     // })
+}
+
+function parseToken(token) {
+    token = Buffer.from(token, 'base64').toString('ascii')
+    return {}
 }
 
 app.listen(3001, () => {
@@ -65,3 +97,4 @@ app.listen(3001, () => {
 // curl -X POST -H "Content-Type: application/json" -d "{\"username\":\"chelovek\", \"password\":\"123\"}" http://localhost:3001/login
 // curl -X POST -H "Content-Type: application/json" -d '{"username":"chelovek","password":"123"}' http://localhost:3001/login
 // curl -H "Authorization: Bearer Y2hlbG92ZWsxMjNiYWI=" http://localhost:3001/users
+// curl -X POST http://localhost:3001/users
